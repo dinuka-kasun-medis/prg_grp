9e38181d885f40065bf5bf20950a9f131311a9d1
Dinuka Kasun Medis <dinuka-kasun-medis@users.noreply.github.com>
2018-11-22 20:10:32 +0530

    added common files

---
+++ application/views/common/script.php
@@ -0,0 +1,7 @@
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jQuery.lightninBox.min.js"></script>
+<script src="js/custom.js"></script>
+<script type="text/javascript">
+$(".lightninBox").lightninBox();
+</script>
