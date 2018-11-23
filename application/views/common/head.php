0a258369b93b8e0f58ee5ba1991dbe154414828f
Dinuka Kasun Medis <dinuka-kasun-medis@users.noreply.github.com>
2018-11-23 11:29:56 +0530

    added css

--- application/views/common/head.php
+++ application/views/common/head.php
@@ -3,9 +3,13 @@
     <title>Designer Portfolio</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="theme-color" content="#03a6f3">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800,900" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <?php $this->load->assets('css/styles.css')?>
-    <?php $this->load->assets('css/bootstrap.min.css')?>
-    <?php $this->load->assets('css/jQuery.lightninBox.css')?>
+    <link rel="stylesheet" href="css/jQuery.lightninBox.css">
+    <!-- <link rel="stylesheet" href="css/styles.css">-->
+    <link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/styles.css')?>">
+    <link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/bootstrap.min.css')?>">
+    <link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/jQuery.lightninBox.css')?>">
+    <link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/owl.carousel.min.css')?>">
 </head>
