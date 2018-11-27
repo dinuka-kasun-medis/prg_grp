d8e4642b3183b70839916733f566641f7ba0a0ef
Dinith Chanuka <30471992+dinithchanuka@users.noreply.github.com>
2018-11-27 16:33:23 +0530

    upload process complete

--- application/controllers/Upload.php
+++ application/controllers/Upload.php
@@ -15,7 +15,7 @@ class Upload extends CI_Controller {
 
     public function do_upload()
     {
-        $config['upload_path']          = './uploads/';
+        $config['upload_path']          = './public/pdf/';
         $config['allowed_types']        = 'pdf|gif|jpg|png';
         $config['max_size']             = 100000;
         $config['max_width']            = 1024;
