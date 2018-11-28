bbf20b632a3e106a54782529e9dae2e53ee0c696
Dinith Chanuka <30471992+dinithchanuka@users.noreply.github.com>
2018-11-28 13:51:39 +0530

    jh

--- application/views/pages/DashboardCustomer.php
+++ application/views/pages/DashboardCustomer.php
@@ -93,6 +93,58 @@
 <link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/admindashboardcss.css')?>">
 <link rel="stylesheet" type="text/css" href="<?=base_url('assets/js/admindashboard.js')?>">
 <link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/mainstyle.css')?>">
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+	
+    <style>
+        * {box-sizing: border-box}
+
+        /* Set height of body and the document to 100% */
+        body, html {
+            height: 100%;
+            margin: 0;
+            font-family: Arial;
+        }
+
+        /* Style tab links */
+        .tablink {
+            background-color: #555;
+            color: white;
+            float: left;
+            border: none;
+            outline: none;
+            cursor: pointer;
+            padding: 14px 16px;
+            font-size: 17px;
+            width: 25%;
+        }
+
+        .tablink:hover {
+            background-color: #777;
+        }
+
+        /* Style the tab content (and add height:100% for full page content) */
+        .tabcontent {
+            color: white;
+            display: none;
+            padding: 10px 20px;
+            height: 100%;
+        }
+
+        #PriceList {background-color: #8A6BCA;}
+        #JobCard {background-color: #8A6BCA;}
+        #Upload {background-color: #8A6BCA;}
+        #Deals {background-color: #8A6BCA;}/*test*/
+    </style>
+    <link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/mainstyle.css')?>">
+</head>
 <body>
 
 <section>
@@ -104,6 +156,9 @@
                 <h3>Price List</h3>
 
         </div>
+<div id="PriceList" class="tabcontent">
+    <?php $this->load->view('forms/PriceList'); ?>        
+</div>
 
         <div class="box second">
             <span class="icon-cont"><i class="fa fa-list"></i></span>
@@ -130,3 +185,6 @@
 
 </section>
 
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha256-5f8gfBsqowzjJe2iM6n0nKciPUp8y+7E=" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+</html>
